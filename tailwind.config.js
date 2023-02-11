1842cd527c48bb495523bde2cbf527282f4b2989
Dave Beazer <hi@davebeazer.dev>
2023-02-11 10:57:48 +0000

    feat: add colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
                 'mintee-dark': '#6FDC87',
                 thyme: '#23903A',
                 'gray-2B2B2B': '#2B2B2B',
+                
+                
+                'true-blue': '#576CA1ff',
+                'cool-gray': '#8F91A8ff',
+                'melon': '#FDB6B8ff',
+                'drab-dark-brown': '#414123ff',
             },
         },
     },
@@ -21,3 +27,4 @@ module.exports = {
     },
     plugins: [],
 }
+
