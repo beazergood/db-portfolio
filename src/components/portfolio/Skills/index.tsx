a8a662ba126eca21fa3bd25d76eb9792014cccdc
Dave Beazer <hi@davebeazer.dev>
2023-02-12 16:38:49 +0000

    fix: color of tech section

--- src/components/portfolio/Skills/index.tsx
+++ src/components/portfolio/Skills/index.tsx
@@ -25,9 +25,9 @@ export const Skills: React.FC<SkillsProps> = () => {
 
   return (
     <>
-      <div className="bg-mintee py-10 my-10">
-        <motion.div className="w-full border- p-4 my-30 py-10  border-green-200 container ">
-          <h1 className="text-2xl text-mintee-dark payton-one">
+      <div className="bg-melon py-10 my-10">
+        <motion.div className="w-full p-4 my-30 py-10 container">
+          <h1 className="text-2xl text-white payton-one">
             Web technologies &amp; tools I work with
           </h1>
           {/* <p className="mt-8 font-medium">Actively engaged with web communities of designers and developers to share knowledge, learn best practices and trends for building web applications.</p> */}
