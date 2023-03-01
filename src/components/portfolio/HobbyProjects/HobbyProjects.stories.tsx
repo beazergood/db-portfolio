9722193a5e30dee1fe8e21da640bee8cd2204d91
Dave Beazer <hi@davebeazer.dev>
2023-03-01 12:02:01 +0000

    fix: updated naming in stories file

--- src/components/portfolio/HobbyProjects/HobbyProjects.stories.tsx
+++ src/components/portfolio/HobbyProjects/HobbyProjects.stories.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { Interests, InterestsProps } from '.'
+import { HobbyProjects } from '.'
 
 export default {
-  title: 'Components/Interests',
-  component: Interests,
+  title: 'Components/HobbyProjects',
+  component: HobbyProjects,
   argTypes: {
     title: { control: 'string' },
   },
 } as Meta
 
-const Template: Story<InterestsProps> = (args) => <Interests {...args} />
+const Template: Story = (args) => <HobbyProjects {...args} />
 
 // Default scenario
 export const Default = Template.bind({})
