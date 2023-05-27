78283bc2ecaf863ffc88f715087c1f62696e04d5
Dave Beazer <hi@davebeazer.dev>
2023-05-27 19:51:28 +0100

    refactor: experience show state

--- src/components/UI/ExperienceCard/index.tsx
+++ src/components/UI/ExperienceCard/index.tsx
@@ -1,19 +1,15 @@
-import { Pill } from '../Pill'
-import { useInView } from 'react-intersection-observer'
-import {
-  useViewportScroll,
-  motion,
-  useTransform,
-} from 'framer-motion'
+import { Pill } from "../Pill";
+import { useInView } from "react-intersection-observer";
+import { useViewportScroll, motion, useTransform } from "framer-motion";
 
 export interface ExperienceCardProps {
-  role: string
-  company: string
-  dates: string
-  pills: Array<any>
-  side: string
-  img: string
-  description: string
+  role: string;
+  company: string;
+  dates: string;
+  pills: Array<any>;
+  side: string;
+  img: string;
+  description: string;
 }
 
 const variantsFromLeft = {
@@ -23,7 +19,7 @@ const variantsFromLeft = {
     scale: 0.65,
     x: -30,
   },
-}
+};
 const variantsFromRight = {
   visible: { opacity: 1, scale: 1, x: 0 },
   hidden: {
@@ -31,28 +27,28 @@ const variantsFromRight = {
     scale: 0.65,
     x: 50,
   },
-}
+};
 
 export const ExperienceCard = ({ ...props }) => {
-  const { scrollY } = useViewportScroll()
-  const y1 = useTransform(scrollY, [0, 300], [0, 200])
-  const y2 = useTransform(scrollY, [0, 300], [0, -100])
+  const { scrollY } = useViewportScroll();
+  const y1 = useTransform(scrollY, [0, 300], [0, 200]);
+  const y2 = useTransform(scrollY, [0, 300], [0, -100]);
 
   const [ref, inView, entry] = useInView({
     /* Optional options */
     threshold: 0.5,
     triggerOnce: true,
-  })
+  });
 
   return (
     <>
-      {props.side == 'left' && (
+      {props.side == "left" && (
         <div className="flex md:contents">
           <div className="flex flex-row-reverse md:contents mr-10">
             <motion.div
               className="col-start-1 col-end-5"
-              transition={{ type: 'spring', stiffness: 100, duration: 2 }}
-              animate={inView ? 'visible' : 'hidden'}
+              transition={{ type: "spring", stiffness: 100, duration: 2 }}
+              animate={inView ? "visible" : "hidden"}
               variants={variantsFromLeft}
               ref={ref}
             >
@@ -64,15 +60,15 @@ export const ExperienceCard = ({ ...props }) => {
           </div>
         </div>
       )}
-      {props.side == 'right' && (
+      {props.side == "right" && (
         <div className="flex md:contents">
           <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
             <TimelineElement />
           </div>
           <motion.div
             className="col-start-6 col-end-10"
-            transition={{ type: 'spring', stiffness: 100, duration: 2 }}
-            animate={inView ? 'visible' : 'hidden'}
+            transition={{ type: "spring", stiffness: 100, duration: 2 }}
+            animate={inView ? "visible" : "hidden"}
             variants={variantsFromRight}
             ref={ref}
           >
@@ -81,8 +77,8 @@ export const ExperienceCard = ({ ...props }) => {
         </div>
       )}
     </>
-  )
-}
+  );
+};
 
 const Card = ({ props }) => {
   const pillsContainer = {
@@ -95,11 +91,11 @@ const Card = ({ props }) => {
         staggerChildren: 0.2,
       },
     },
-  }
+  };
   const listItem = {
     hidden: { opacity: 0 },
     show: { opacity: 1 },
-  }
+  };
   return (
     <div className="bg-white p-6 rounded-xl my-4 ml-auto shadow-md block">
       <h3 className="font-semibold mb-1 text-sm uppercase font-mono text-gray-400">
@@ -108,9 +104,9 @@ const Card = ({ props }) => {
       <h1 className="leading-tight text-left text-2xl text-gray-600 mb-4">
         {props.role}
       </h1>
-      {/* {props.description && (
+      {props.description && (
         <p className="text-left text-gray-500">{props.description}</p>
-      )} */}
+      )}
       <motion.ul
         className="py-2 flex flex-wrap p-x-2 my-2 w-full relative top-1"
         variants={pillsContainer}
@@ -123,14 +119,14 @@ const Card = ({ props }) => {
             <motion.li key={i} variants={listItem}>
               <Pill title={p} />
             </motion.li>
-          )
+          );
         })}
       </motion.ul>
 
       <p className="leading-tight text-gray-400 text-right">{props.dates}</p>
     </div>
-  )
-}
+  );
+};
 
 const TimelineElement = () => {
   return (
@@ -146,5 +142,5 @@ const TimelineElement = () => {
         className="w-6 h-6 absolute top-1/2 -mt-10 rounded-full bg-white stroke-2 border-4 border-melon shadow"
       ></motion.div>
     </>
-  )
-}
+  );
+};
