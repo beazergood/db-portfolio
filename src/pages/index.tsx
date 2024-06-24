1f093d80a8a3a228947f78516e49738e346d5204
Dave Beazer <davebeazer@me.com>
2024-06-24 23:03:48 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,7 @@ export default function Home() {
       </Head>
         <Hero />
         <About />
-        <Experience/>
-        <HobbyProjects />
+       
         <Skills />
         <Contact />
     </>
